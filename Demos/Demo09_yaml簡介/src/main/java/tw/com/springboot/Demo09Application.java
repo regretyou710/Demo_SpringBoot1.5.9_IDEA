1aeb3fa44366ca4665fbfaf058ab237519e87951
eason <regretyou@gmail.com>
2022-09-18 12:56:38 +0800

    202209181256

---
+++ Demos/Demo09_yaml簡介/src/main/java/tw/com/springboot/Demo09Application.java
@@ -0,0 +1,13 @@
+package tw.com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo09Application.class, args);
+	}
+
+}

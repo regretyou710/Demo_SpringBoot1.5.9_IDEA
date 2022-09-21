78ffcfa2eb18a88bc99ad030656cf934846bb79c
eason <regretyou@gmail.com>
2022-09-21 14:20:39 +0800

    202209211420(Demo17_配置文件的加載位置)

---
+++ Demos/Demo17_配置文件的加載位置3/src/main/java/tw/com/springboot/Demo17Application.java
@@ -0,0 +1,13 @@
+package tw.com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo17Application.class, args);
+	}
+
+}

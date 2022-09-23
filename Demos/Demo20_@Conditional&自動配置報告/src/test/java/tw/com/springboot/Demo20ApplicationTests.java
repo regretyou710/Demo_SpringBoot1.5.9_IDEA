edd261948d590b62ffdfeb898a01db54f8deb8a3
eason <regretyou@gmail.com>
2022-09-23 19:41:25 +0800

    202209231941(Demo20_@Conditional&自動配置報告)

---
+++ Demos/Demo20_@Conditional&自動配置報告/src/test/java/tw/com/springboot/Demo20ApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

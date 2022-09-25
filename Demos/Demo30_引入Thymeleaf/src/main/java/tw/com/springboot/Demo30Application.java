9313d090ea8292a2943d838e0dcceae9b3d96077
eason <regretyou@gmail.com>
2022-09-25 12:52:22 +0800

    202209251252(Demo30_引入Thymeleaf)

---
+++ Demos/Demo30_引入Thymeleaf/src/main/java/tw/com/springboot/Demo30Application.java
@@ -0,0 +1,13 @@
+package tw.com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo30Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo30Application.class, args);
+	}
+
+}

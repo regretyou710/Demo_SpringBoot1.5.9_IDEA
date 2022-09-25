20d3a14bd72bd08c6602593756acd17a3e78978d
eason <regretyou@gmail.com>
2022-09-25 10:05:01 +0800

    202209251004(Demo29_webjars&靜態資源映設規則)

---
+++ Demos/Demo29_webjars&靜態資源映設規則1/src/main/java/tw/com/springboot/Demo291Application.java
@@ -0,0 +1,13 @@
+package tw.com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo291Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo291Application.class, args);
+	}
+
+}

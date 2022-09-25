9139401c8783d1b646581a4151b6497b94402d46
eason <regretyou@gmail.com>
2022-09-25 14:47:55 +0800

    202209251447(Demo31_Thymeleaf語法)

---
+++ Demos/Demo31_Thymeleaf語法/src/main/java/tw/com/springboot/controller/HelloController.java
@@ -0,0 +1,26 @@
+package tw.com.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello world";
+    }
+
+    //查出用戶數據，在頁面展示
+    @RequestMapping("/success")
+    public String success(Map<String, Object> map) {
+        map.put("hello", "<h1>你好</h1>");
+        map.put("users", Arrays.asList("zhangsan", "lisi", "wangwu"));
+        return "success";
+    }
+}

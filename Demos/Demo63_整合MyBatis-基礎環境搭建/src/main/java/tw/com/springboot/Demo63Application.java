8211287de844a44ad4796395e7173ab0486e863b
eason <regretyou@gmail.com>
2022-10-10 10:43:08 +0800

    202210101042(Demo63_整合MyBatis-基礎環境搭建)

---
+++ Demos/Demo63_整合MyBatis-基礎環境搭建/src/main/java/tw/com/springboot/Demo63Application.java
@@ -0,0 +1,13 @@
+package tw.com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo63Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo63Application.class, args);
+	}
+
+}
---
+++ Demos/Demo63_整合MyBatis-基礎環境搭建/備忘錄_demo63.md
@@ -0,0 +1,19 @@
+## 整合MyBatis
+
+```xml
+		<dependency>
+			<groupId>org.mybatis.spring.boot</groupId>
+			<artifactId>mybatis-spring-boot-starter</artifactId>
+			<version>1.3.1</version>
+		</dependency>
+```
+
+![](images/搜狗截圖20180305194443.png)
+
+步驟：
+
+​	1）、配置數據源相關屬性（見上一節Druid）
+
+​	2）、給數據庫建表
+
+​	3）、創建JavaBean
